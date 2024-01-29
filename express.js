70d8fade9975a043be3b1ac90dd92c0981416293
gukihuman <sokolovivanf@gmail.com>
2024-01-29 23:33:10 +0500

    add dotenv and redux

--- express.js
+++ express.js
@@ -2,22 +2,59 @@ import express from "express"
 import path from "path"
 import { fileURLToPath } from "url"
 import { dirname } from "path"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 5050
+const PORT = 5050
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+fetchShopifyData().then((data) => {
+    console.log(data)
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")))
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename)
 
-// Handle requests
-app.get("*", (req, res) => {
-    console.log(req)
-    res.sendFile(path.join(__dirname, "client/build/index.html"))
+    // Serve client static files
+    app.use(express.static(path.join(__dirname, "client/build")))
+    app.get("*", (req, res) => {
+        console.log(req)
+        res.sendFile(path.join(__dirname, "client/build/index.html"))
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`)
+    })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+async function fetchShopifyData() {
+    const accessToken = process.env.SHOPIFY_ACCESS_TOKEN
+    const storeName = process.env.SHOPIFY_STORE_NAME
+
+    const graphqlURL = `https://${storeName}/admin/api/2024-01/graphql.json`
+    const query = `{
+        products (first: 3) {
+            edges {
+                node {
+                    id
+                    bodyHtml
+                }
+            }
+        }
+    }`
+
+    try {
+        const response = await fetch(graphqlURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Shopify-Access-Token": accessToken,
+            },
+            body: JSON.stringify({ query }),
+        })
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error("Error fetching data from Shopify:", error)
+    }
+}
